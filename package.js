4de22db6e94d0cbb9f665078ff706cceb42c6306
Alastair Dant <alastair@recursiveflow.com>
2015-12-04 18:19:04 -0800

    Bump to version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "riffyn:autocomplete",
   summary: "Client/server autocompletion designed for Meteor's collections and reactivity",
-  version: "0.4.15",
+  version: "0.4.16",
   git: "https://github.com/RiffynInc/meteor-autocomplete.git"
 });
 
